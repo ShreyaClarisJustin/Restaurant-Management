019a69b47c5024d61dbfe5aca6689d15badbfdb8
ShreyaClarisJustin <shreya.claris@gmail.com>
2026-06-19 15:14:28 +0530

    updated index.html

--- app.py
+++ app.py
@@ -15,6 +15,8 @@
 
 
 from flask import Flask, render_template
+from flask import request
+
 import mysql.connector
 
 app = Flask(__name__)
@@ -42,6 +44,62 @@ def menu():
         'menu.html',
         foods=foods
     )
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+
+
+
+from flask import request, redirect, url_for
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+        password = request.form['password']
+
+        sql = """
+        SELECT * FROM users
+        WHERE email=%s AND password=%s
+        """
+
+        cursor.execute(sql, (email, password))
+
+        user = cursor.fetchone()
+
+        if user:
+            return redirect('/menu')
+        else:
+            return "Invalid Email or Password"
+
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+# from flask import request
+
+@app.route('/register', methods=['POST'])
+def register_user():
+
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    password = request.form['password']
+
+    sql = """
+    INSERT INTO users(name,email,phone,password)
+    VALUES(%s,%s,%s,%s)
+    """
+
+    cursor.execute(sql, (name,email,phone,password))
+    db.commit()
+
+    return render_template('login.html')
 
 @app.route('/admin')
 def admin():
